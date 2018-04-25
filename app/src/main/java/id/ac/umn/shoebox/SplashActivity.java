2bd9674b33803717ecad8ad14488c6e3067e2107
Stefanus K <stefanus.kurniawan@student.umn.ac.id>
2018-04-25 13:56:41 +0700

    Firebase Config

--- app/src/main/java/id/ac/umn/shoebox/SplashActivity.java
+++ app/src/main/java/id/ac/umn/shoebox/SplashActivity.java
@@ -4,9 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.Window;
-
-import sun.security.tools.keytool.Main;
 
 public class SplashActivity extends Activity {
 
